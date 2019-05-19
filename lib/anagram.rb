042938b4dfa4eeee9441b6688c8872ce1ad40852
Renee Nordholm <reneenordholm@gmail.com>
2019-05-19 19:25:03 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,20 @@
 class Anagram
+  attr_accessor :word 
   
-  attr_accessor :word
-
   def initialize(word)
-    @word = word
-  end
+    @word = word 
+  end 
   
   def match(array)
+    new_array = []
     array.each do |item|
-      item.split("") 
-  end
-end
+      if item.split("").sort == @word.split("").sort  
+        new_array << item 
+      else
+        new_array
+    end 
+    end 
+    new_array 
+  end 
+
+end 
